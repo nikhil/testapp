03ff7113917846ce1eb2866c875146e5fcf7fa74
Nikhil Kumar <nikhilkumar516@gmail.com>
2015-04-12 18:16:35 -0400

    disabled api requesting

--- hi.py
+++ hi.py
@@ -1,9 +1,9 @@
 import requests
 
 text = 'Hello how are you?'
-params3 = {'apikey': 'd8894db2dd60aed653e7bd91ea854ce91f46ec85', 'text': text, 'outputMode': 'json'}
-analyzedString = requests.get('http://access.alchemyapi.com/calls/text/TextGetTextSentiment',params=params3).json()
-print analyzedString['docSentiment']['score']
+#params3 = {'apikey': 'd8894db2dd60aed653e7bd91ea854ce91f46ec85', 'text': text, 'outputMode': 'json'}
+#analyzedString = requests.get('http://access.alchemyapi.com/calls/text/TextGetTextSentiment',params=params3).json()
+#print analyzedString['docSentiment']['score']
 
 
 
