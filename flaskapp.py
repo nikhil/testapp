84ddf6bdc47798daa5b76a2299151449ecfc4d9e
Nikhil Kumar <nikhilkumar516@gmail.com>
2015-04-12 18:14:04 -0400

    added things

--- flaskapp.py
+++ flaskapp.py
@@ -18,7 +18,7 @@ def my_form_post():
     text = request.form['text']
     params3 = {'apikey': 'd8894db2dd60aed653e7bd91ea854ce91f46ec85', 'text': text, 'outputMode': 'json'}
     analyzedString = requests.get('http://access.alchemyapi.com/calls/text/TextGetTextSentiment',params=params3).json()
-    return analyzedString['score']
+    return analyzedString['docSentiment']['score']
 
 @app.route('/<path:resource>')
 def serveStaticResource(resource):
--- hi.py
+++ hi.py
@@ -3,7 +3,7 @@ import requests
 text = 'Hello how are you?'
 params3 = {'apikey': 'd8894db2dd60aed653e7bd91ea854ce91f46ec85', 'text': text, 'outputMode': 'json'}
 analyzedString = requests.get('http://access.alchemyapi.com/calls/text/TextGetTextSentiment',params=params3).json()
-print analyzedString['score']
+print analyzedString['docSentiment']['score']
 
 
 
